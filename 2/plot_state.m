e3dbd51f940a5f8b130b01e35bd5816905f8a20b
filippottaviani <filippo.ottaviani.tr@gmail.com>
2025-12-11 00:24:02 +0100

    Fix plot es 2

--- 2/plot_state.m
+++ 2/plot_state.m
@@ -1,11 +1,12 @@
 function plot_state(x_real, x_hat, t)
     graphics_toolkit("gnuplot"); # fix per i grafici
+    clf
     figure('Name', 'Regolazione dall uscita del pendolo linearizzato');
 
     subplot(1,2,1);
-    plot(t, x_real(:,1), 'b', 'LineWidth', 1.5); 
+    plot(t, x_real(:,1), 'b', 'LineWidth', 2); 
     hold on;
-    plot(t, x_real(:,3), 'r', 'LineWidth', 1.5);
+    plot(t, x_real(:,3), 'r', 'LineWidth', 2);
     title('Stati del sistema');
     xlabel('Tempo [s]');
     ylabel('Stato');
@@ -13,7 +14,7 @@ function plot_state(x_real, x_hat, t)
     grid on;
 
     subplot(1,2,2);
-    plot(t, x_real(:,3) - x_hat(:,3), 'k', 'LineWidth', 1.5);
+    plot(t, x_real(:,3) - x_hat(:,3), 'k', 'LineWidth', 2);
     title('Errore di stima angolo');
     xlabel('Tempo [s]');
     ylabel('Errore [rad]');
